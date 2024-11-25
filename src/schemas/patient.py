9dd96639e6da3543225b9f409c7747e0020ea78d
fedemelo <f.melo@uniandes.edu.co>
2024-11-24 22:36:32 -0500

    Implement patient schema

---
+++ src/schemas/patient.py
@@ -0,0 +1,71 @@
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class PatientBase(BaseModel):
+    """
+    Base schema for the Patient model.
+
+    Attributes
+    ----------
+    cedula : int
+        Unique identification number for the patient.
+        Ideally a Colombian cedula number.
+    name : Optional[str], optional
+        Name of the patient.
+    """
+
+    cedula: int
+    name: Optional[str] = None
+
+
+class PatientCreate(PatientBase):
+    """
+    Schema for creating a Patient instance.
+    """
+    doctor_email: str
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "cedula": 123456789,
+                "name": "John Smith",
+                "doctor_email": "doctor@example.com",
+            }
+        }
+
+
+class PatientUpdate(BaseModel):
+    """
+    Schema for updating a Patient instance.
+
+    Attributes
+    ----------
+    name : Optional[str], optional
+        Updated name of the patient.
+    """
+
+    name: Optional[str] = None
+    doctor_email: Optional[str] = None
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "name": "Jane Smith",
+            }
+        }
+
+
+class PatientResponse(PatientBase):
+    """
+    Schema for the Patient response.
+    """
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "cedula": 123456789,
+                "name": "John Smith",
+            }
+        }
